b96f5fb2456b610378af4591bf88275896966032
Raghav <draghav@deloitte.com>
2025-06-08 23:34:51 +0530

    task2 pipeline

---
+++ terraform/s3_secure/variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket"
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region"
+}
